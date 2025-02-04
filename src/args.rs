122044fa3c0023cd4b379b7f9d63131954055032
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-02-04 13:09:19 +0000

    fix unwrap in args module

--- src/args.rs
+++ src/args.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use clap::{self, Parser};
 
 use crate::config::get_config_dir;
@@ -5,7 +7,10 @@ use crate::config::get_config_dir;
 pub fn about() -> String {
     let author = env!("CARGO_PKG_AUTHORS");
 
-    let config_dir = get_config_dir().unwrap().display().to_string();
+    let config_dir = get_config_dir()
+        .unwrap_or_else(|_| PathBuf::from("$HOME/.config/ccol/"))
+        .to_string_lossy()
+        .into_owned();
 
     format!(
         "Authors: {author}
